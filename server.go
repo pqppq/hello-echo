af969a2f10a5d5f07e48f844cf0070a48cd0c460
pqppq <yutatsu0119@gmail.com>
2022-05-16 00:52:55 +0900

    add routing /users/:id, /show, /save

--- server.go
+++ server.go
@@ -11,5 +11,31 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, workd!")
 	})
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// e.GET('/users/:id', getUser)
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+// e.GET("/show", show)
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:" + team + "\tmember:" + member)
+}
+
+// e.POST("/save", save)
+func save(c echo.Context) error {
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+ name + "\temail:" + email)
 }
