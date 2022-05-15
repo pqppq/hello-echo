bfa2cda642a9db5f41d94359d16d262b96be4bfd
pqppq <yutatsu0119@gmail.com>
2022-05-16 01:16:55 +0900

    handling request

--- server.go
+++ server.go
@@ -8,12 +8,25 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type User struct {
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
+	Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, workd!")
 	})
 	e.GET("/users/:id", getUser)
+	e.POST("/users", func(c echo.Context) error { u := new(User)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+		return c.JSON(http.StatusCreated, u)
+		// or
+		// return c.XML(http.StatusCreated, u)
+	})
 	e.GET("/show", show)
 	e.POST("/save", save)
 
